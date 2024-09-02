076efa2a7d8cbbdeac6fd3353258fce6a23c6c74
Inf1n1ty75 <volkandu75@gmail.com>
2024-09-02 16:29:40 +0200

    Add comments to explain the purpose of key operations
    - Added comments to explain the initialization of epoll for managing multiple file descriptors.
    - Included comments to clarify the purpose of setting socket options and binding the socket.
    - Added comments to describe the importance of accepting connections and modifying epoll events.

--- src/polling.cpp
+++ src/polling.cpp
@@ -19,28 +19,22 @@
 
 #include <iostream>
 
-// bool main_loop = true;
-
-// void sigint(int signum)
-// {
-//   (void) signum;
-//   main_loop = false;
-// }
-
 int main() {
   int event_count;
   char read_buffer[READ_SIZE + 1];
-  struct epoll_event event, events[MAX_EVENTS];
+  struct epoll_event events[MAX_EVENTS];
+
+  // Initialize epoll to efficiently manage multiple file descriptors for I/O
+  // events.
   int epoll_fd = epoll_create1(0);
-
-  // signal(SIGINT, sigint);
-
   if (epoll_fd == -1) {
     std::cerr << NRED << strerror(errno) << RED << ": epoll_create1() failed."
               << RESET << std::endl;
     return 1;
   }
 
+  // Create a socket for network communication using IPv4 and TCP.
+  // This socket will be used to listen for incoming connections.
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd == -1) {
     std::cerr << NRED << strerror(errno) << RED << ": socket() failed." << RESET
@@ -48,6 +42,8 @@ int main() {
     return 1;
   }
 
+  // By doing this, we can use the same port after relaunching the web server.
+  // Without it, the port remains in use for a while.
   int b = true;
   setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &b, sizeof(int));
 
@@ -56,18 +52,27 @@ int main() {
   sockaddr.sin_addr.s_addr = INADDR_ANY;
   sockaddr.sin_port = htons(9999);
 
+  // Ensure the server can accept incoming connections by binding the socket to
+  // the specified IP address and port. This step is crucial for the server to
+  // listen for and accept client requests on the designated network interface.
   if (bind(sockfd, (struct sockaddr *)&sockaddr, sizeof(sockaddr)) == -1) {
     std::cerr << NRED << strerror(errno) << RED << ": bind() failed." << RESET
               << std::endl;
     return 1;
   }
 
+  // Prepare the socket to accept incoming connection requests by setting it to
+  // a listening state. This is essential for the server to handle multiple
+  // client connections concurrently.
   if (listen(sockfd, 42) == -1) {
     std::cerr << NRED << strerror(errno) << RED << ": listen() failed." << RESET
               << std::endl;
     return 1;
   }
 
+  // Accept an incoming connection to establish communication with a client.
+  // This step is crucial for the server to handle client requests and provide
+  // responses.
   socklen_t addrlen = sizeof(sockaddr);
   int connection =
       accept(sockfd, (struct sockaddr *)&sockaddr, (socklen_t *)&addrlen);
@@ -77,6 +82,11 @@ int main() {
     return 1;
   }
 
+  // Configure the epoll event to monitor the new connection for incoming data
+  // and edge-triggered events. This setup is essential for efficiently
+  // detecting and handling I/O events on the connection, ensuring the server
+  // can respond promptly to client requests.
+  struct epoll_event event;
   event.events = EPOLLIN | EPOLLET;
   event.data.fd = connection;
 
@@ -87,15 +97,17 @@ int main() {
     return 1;
   }
 
-  // if (req.get_param("Connection") == "keep-alive")
-  //   response.add_param("Connection", "keep-alive");
-
   while (1) {
     std::cout << YELLOW << "Polling for input..." << RESET << std::endl;
     event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
 
     std::cout << GREEN << event_count << " ready events." << RESET << std::endl;
     for (int i = 0; i < event_count; i++) {
+
+      // Check if the current event indicates that there is data to read on the
+      // file descriptor.
+      // This is crucial for processing incoming data from the client, ensuring
+      // the server can handle read operations when data is available.
       if ((events[i].events & EPOLLIN) == EPOLLIN) {
         std::cout << CYAN << "Reading file descriptor: " << events[i].data.fd
                   << RESET << std::endl;
@@ -108,6 +120,9 @@ int main() {
         std::cout << PURPLE << "bytes read: " << bytes_read << RESET
                   << std::endl;
 
+        // Modify the event to monitor the file descriptor for outgoing data.
+        // This is necessary to prepare the server to send a response back to
+        // the client after reading the incoming data.
         struct epoll_event event;
         event.events = EPOLLOUT | EPOLLET;
         event.data.fd = events[i].data.fd;
@@ -121,6 +136,9 @@ int main() {
         }
       }
 
+      // Check if the current event indicates that the file descriptor is ready
+      // for writing. This is essential for sending data to the client, ensuring
+      // the server can handle write operations when the socket is ready.
       else if ((events[i].events & EPOLLOUT) == EPOLLOUT) {
         Response response = Response::httpcat(401);
         response.add_param("Connection", "keep-alive");
@@ -134,6 +152,9 @@ int main() {
                     << RESET << std::endl;
         }
 
+        // Modify the event to monitor the file descriptor for outgoing data.
+        // This is necessary to prepare the server to send a response back to
+        // the client after reading the incoming data.
         struct epoll_event event;
         event.events = EPOLLIN | EPOLLET;
         event.data.fd = events[i].data.fd;
